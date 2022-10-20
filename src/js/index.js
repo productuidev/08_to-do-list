5fe94275e718961b486ba6da38c2a56ec4e0ff55
productuidev <productuidev@gmail.com>
2022-10-21 03:28:30 +0900

    html markup and style.scss (customized)

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
-import "../scss/style.scss"
+import '@fortawesome/fontawesome-free/js/all.min.js';
+import '../scss/style.scss';
 
 let a = 1000;
 let b = 2000;
 
-console.log(a+b);
+console.log(a + b);
