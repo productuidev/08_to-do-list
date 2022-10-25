9f27e2db6de14cbe063b2cf8260ddc1b5dff3eb4
productuidev <productuidev@gmail.com>
2022-10-26 07:20:58 +0900

    add editTodo, saveTodo and completeTodo

--- src/js/index.js
+++ src/js/index.js
@@ -21,14 +21,27 @@ class TodoList {
     this.todoListEl.addEventListener('click', this.onClickTodoList.bind(this));
   }
 
+  // todo btn 이벤트
   onClickTodoList(event) {
     const { target } = event;
     const btn = target.closest('button');
+
+    // btn이 null일 때의 조건 추가
+    // console.log(btn);
+    if (!btn) return; // = false
+
     if (btn.matches('#delete-btn')) {
       this.deleteTodo(target);
+    } else if (btn.matches('#edit-btn')) {
+      this.editTodo(target);
+    } else if (btn.matches('#save-btn')) {
+      this.saveTodo(target);
+    } else if (btn.matches('#complete-btn')) {
+      this.completeTodo(target);
     }
   }
 
+  // 할일 삭제
   deleteTodo(target) {
     const todoDiv = target.closest('.todo');
     if (window.confirm('정말 삭제하시겠습니까?')) {
@@ -39,6 +52,32 @@ class TodoList {
     todoDiv.classList.add('delete');
   }
 
+  // 할일 수정
+  editTodo(target) {
+    const todoDiv = target.closest('.todo');
+    const todoInputEl = todoDiv.querySelector('input');
+    todoInputEl.readOnly = false;
+    todoInputEl.focus();
+    todoDiv.classList.add('edit');
+  }
+
+  // 할일 수정 후 저장
+  saveTodo(target) {
+    const todoDiv = target.closest('.todo');
+    todoDiv.classList.remove('edit');
+
+    // 수정된 값 가져오기 (저장된 값을 가져오는 것은 이후 localStorage에서..)
+    const todoInputEl = todoDiv.querySelector('input');
+    todoInputEl.readOnly = true;
+  }
+
+  // 할일 완료 체크
+  completeTodo(target) {
+    const todoDiv = target.closest('.todo');
+    todoDiv.classList.toggle('done');
+  }
+
+  // 할일 추가
   onClickAddBtn() {
     if (this.todoInputEl.value.length === 0) {
       alert('내용을 입력해주세요');
@@ -48,6 +87,7 @@ class TodoList {
     this.createTodoElement(this.todoInputEl.value);
   }
 
+  // todo 동적생성
   createTodoElement(value) {
     const todoDiv = document.createElement('div');
     todoDiv.classList.add('todo');
